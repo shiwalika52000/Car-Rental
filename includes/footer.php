318b890e0ea0f65954cc54b01ad2bf95e7dee026
shiwalika52000 <shiwalika3@gmail.com>
2024-06-18 19:03:45 +1200

    car rental project

---
+++ includes/footer.php
@@ -0,0 +1,19 @@
+	<li class="about">
+					<p>shiwaworlds team is an organized company that rents cars and other vehicles to clients at lower costs. We we are here to serve every indian Citizen</p>
+					<ul>
+						<li><a href="http://facebook.com/shiwaworldsin" class="facebook" target="_blank"></a></li>
+						<li><a href="http://twitter.com/shiwaworlds32" class="twitter" target="_blank"></a></li>
+						<li><a href="http://plus.google.com/shiwaworlds32" class="google" target="_blank"></a></li>
+						<li><a href="#" class="skype"></a></li>
+					</ul>
+				</li>
+			</ul>
+		</div>
+
+		<div class="copyrights wrapper">
+			Copyright &copy; <?php echo date("Y")?> All Rights Reserved | Designed by <a href= "#">shiwaworlds</a>
+		</div>
+	</footer><!--  end footer  -->
+	
+</body>
+</html>
